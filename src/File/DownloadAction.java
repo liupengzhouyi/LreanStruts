3afe58442336e36798065c896dba739e61249e43
liupeng <LIUPENG.0@outlook.com>
2018-12-19 11:43:43 +0800

    文件下载Bug版本

--- src/File/DownloadAction.java
+++ src/File/DownloadAction.java
@@ -1,5 +1,38 @@
 package File;
 
-public class DownloadAction {
+import com.opensymphony.xwork2.ActionSupport;
+import org.apache.struts2.ServletActionContext;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+public class DownloadAction extends ActionSupport {
+
+    private InputStream fileInput;
+
+    private String fileName;
+
+    public InputStream getFileInput() {
+        return ServletActionContext.getServletContext().getResourceAsStream("uploead/" + fileName);
+    }
+
+    public void setFileInput(InputStream fileInput) {
+        this.fileInput = fileInput;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String execute() throws Exception {
+        System.out.println("push:" + ServletActionContext.getServletContext().getResourceAsStream("uploead/" + fileName));
+
+        fileInput = ServletActionContext.getServletContext().getResourceAsStream("uploead/" + fileName);
+
+        return SUCCESS;
+    }
 }
---
+++ web/File/dowload.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: liupeng
+  Date: 2018-12-19
+  Time: 11:10
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <a href="dowLoad.action?fileName=屏幕快照 2018-12-18 下午6.56.54.png">屏幕快照 2018-12-18 下午6.56.54.png</a>
+</body>
+</html>
